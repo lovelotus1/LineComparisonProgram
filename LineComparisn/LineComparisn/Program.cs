56a654c3a77bba1cf244dce7ef1229756a2034cd
lovelotus <opera.prem@gmail.com>
2022-07-17 22:27:35 +0530

    Added Line Comaprison.cs File to Find Line Length

--- LineComparisn/LineComparisn/Program.cs
+++ LineComparisn/LineComparisn/Program.cs
@@ -1,3 +1,8 @@
-﻿
+﻿using LineComparison;
+
+
 
 Console.WriteLine("Welcome to Line Comparison Computation Program");
+LineComparison.LineComparison lineComparison = new LineComparison.LineComparison();
+lineComparison.LineLength();
+Console.ReadLine();
